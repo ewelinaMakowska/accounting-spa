1c4f8aa7ade6680f365a14ad14588a6b1655d6ae
Ewelina Makowska <ewelina.makowska0@gmail.com>
2020-01-25 20:33:30 +0100

    mysql dependency

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,14 +1,16 @@
+const myqsl = require('mysql2');
+
 module.exports = {
     port: 3000,
     db: {
         database: process.env.DB_NAME || 'accounting_companies',
-        user: process.env.DB_USER || 'accounting_app',
+        user: process.env.DB_USER || 'root',
         password: process.env.DB_PASSWORD || 'A5@@nbd',
         options: {
-            dialect: process.env.DB_DIALECT || 'sqlite',
+            dialect: process.env.DB_DIALECT || 'mysql',
             host: process.env.DB_HOST || 'localhost',
             operatorsAliases: false,
-            storage: './accounting_companies.sqlite'
+            storage: './accounting_companies.sqlcd'
         }
     }
 }
