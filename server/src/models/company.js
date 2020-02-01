b1e1bf9a15c7787ebb714a397fa4e5d1cff83d91
Ewelina Makowska <ewelina.makowska0@gmail.com>
2020-02-01 14:51:36 +0100

    Revert "Revert "Revert "Revert "Revert "Revert "company model""""""
    This reverts commit e837c5ba3ae119bf889849e0916cad54917a5693.

--- server/src/models/company.js
+++ server/src/models/company.js
@@ -1,12 +1,15 @@
 // module.exports = (sequelize, DataTypes) => {
 //     const Company = sequelize.define('company', {
-//             id: {type: DataTypes.STRING(10), primaryKey: true, allowNull: false},
-//             name: {type: DataTypes.STRING(60), allowNull: false},
-//             price: {type: DataTypes.DOUBLE(5,2), allowNull: false}
+//             id: {type: DataTypes.INT(10), primaryKey: true, allowNull: false},
+//             name: {type: DataTypes.STRING(45), allowNull: false},
+//             price: {type: DataTypes.DOUBLE(5,2), allowNull: false},
+//             logo: {type: DataTypes.STRING(45), allowNull: true},
+//             description: {type: DataTypes.TEXT(), allowNull: true},
 //     })
 
 //     return Company
 
-
-
 // }
+
+//id name price logo description
+//int varchar double(5,2) varchar text
