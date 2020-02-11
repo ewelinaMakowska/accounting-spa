a368aab54f3d596496eecc89f17a04f1ed5465c5
Ewelina Makowska <ewelina.makowska0@gmail.com>
2020-02-11 17:24:07 +0100

    null state

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,20 +4,31 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
+  
+  // state: {
+  //   loadedOffices: [
+  //     { id: '1', name: 'Ksiegowosc', price: 100 },
+  //     { id: '2', name: 'BMW', price: 80 },
+  //     { id: '3', name: 'Volkswagen', price: 75 },
+  //     { id: '4', name: 'Audi', price: 85 }
+  //   ],
+  //   user: {
+  //     id: '624',
+  //     favouriteOffices: ['2', '3']
+  //   }
+      
+    
+  // },
+
   state: {
     loadedOffices: [
-      { id: '1', name: 'Ksiegowosc', price: 100 },
-      { id: '2', name: 'BMW', price: 80 },
-      { id: '3', name: 'Volkswagen', price: 75 },
-      { id: '4', name: 'Audi', price: 85 }
     ],
     user: {
       id: '624',
       favouriteOffices: ['2', '3']
     }
-      
-    
   },
+
   mutations: {
   },
   actions: {
