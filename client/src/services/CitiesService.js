6c7b0cc4f3d1082c3a67a88eecef212a45f9e644
Ewelina Makowska <ewelina.makowska0@gmail.com>
2020-05-23 10:50:01 +0200

    cities service

--- client/src/services/CitiesService.js
+++ client/src/services/CitiesService.js
@@ -5,6 +5,9 @@ export default {
   getAllCities() {
     return Api.get('cities');
     //console.log("sent to server code"); 
+  },
+  getFilteredLimited(value) {
+    return Api.get(`citiesFilteredLimited/?name=${value}`, value);
   }
 
 
