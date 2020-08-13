554bf36da212622222f302e24f91a98dc0d6ae32
Ewelina Makowska <ewelina.makowska0@gmail.com>
2020-08-13 17:26:19 +0200

    test commit

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,6 +2,6 @@
 <div>
   Hello World!
 <br/><br/>
-  <a href="/search?city=&amp;sort=&amp;page=1">Wyszukaj biuro dla siebie</a>
+  <a href="/search?city=&amp;sort=&amp;page=1">Wyszukaj biuro dla siebie</a>test
   </div>
 </template>
