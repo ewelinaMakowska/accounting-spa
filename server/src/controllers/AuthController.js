99522ee0a03999ab49e911b5d7468b55590bc3d0
Ewelina Makowska <ewelina.makowska0@gmail.com>
2020-08-26 21:07:27 +0200

    register endpoint II

--- server/src/controllers/AuthController.js
+++ server/src/controllers/AuthController.js
@@ -2,7 +2,7 @@
 //const { validationResult } = require('express-validator/check')
 
 module.exports = {
-  async registerUser(req, res, next) {
-    
+  async registerUser(req, res) {
+    res.sendStatus(200).json({ message: 'OK!'});
   }
 } //module exports end
