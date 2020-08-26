6efe2b0ff4b5afc1b714d301e8338d01d719fb67
Ewelina Makowska <ewelina.makowska0@gmail.com>
2020-08-26 20:00:13 +0200

    basic register form

--- client/src/views/RegistrationPage.vue
+++ client/src/views/RegistrationPage.vue
@@ -4,7 +4,16 @@
             <div class="container">
                 <div class="row">
                     <div class="col-sm-12">
-                      formularz rejestracji
+                      <b>Registration</b><br/><br/>
+                        <form @submit="submitForm($event)" novalidate method="post">
+                            <label for="form__name">First Name:</label><input id="form__name" name="name" type="text"/><br/><br/>
+                            <label for="form__name">Last Name:</label><input id="form__name" name="name" type="text"/><br/><br/>
+                            <label for="form__email">E-mail:</label><input id="form__email" name="email" type="email"/><br/><br/>
+                            <label for="form__email">Password:</label><input id="form__password" name="password" type="password"/><br/><br/>
+                            <label for="form__email">Confirm Password:</label><input id="form__password" name="password" type="password"/><br/><br/>
+                          
+                            <input type="submit" value="Send" />
+                        </form>
                     </div>
                 </div>
             </div>
@@ -18,6 +27,17 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+    input[type="text"],
+    input[type="email"],
+    input[type="password"] {
+        border: 1px solid #333;
+        margin-left: 20px;
+    }
+
+    input[type="submit"] {
+        background: lightblue;
+    }
 
 </style>
