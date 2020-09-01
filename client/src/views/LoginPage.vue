520f82f6c75980935f09e793c85537165a41dead
Ewelina Makowska <ewelina.makowska0@gmail.com>
2020-09-01 21:20:11 +0200

    login page

--- client/src/views/LoginPage.vue
+++ client/src/views/LoginPage.vue
@@ -4,9 +4,20 @@
             <div class="container">
                 <div class="row">
                     <div class="col-sm-12">
-                      formularz logowania
-                      Nie masz jeszcze konta? <br/>
-                      <a href="/register" >Zarejestruj się!</a>
+                        <b>LogIn</b><br/><br/><br/>
+                        <form @submit="submitForm($event)" novalidate > <!-- method="post" -->
+    
+                            <label for="form__email">E-mail:</label>
+                            <input id="form__email" name="email" type="email" v-model.trim="userData.email"/><br/><br/>
+
+                            <label for="form__email">Password:</label>
+                            <input id="form__password" name="password" type="password" v-model="userData.password"/><br/><br/>
+                            
+                            <input type="submit" value="Send" />
+                        </form>
+                        <br/>
+                        Nie masz jeszcze konta? <br/>
+                        <a href="/register" >Zarejestruj się!</a>
                     </div>
                 </div>
             </div>
@@ -16,10 +27,46 @@
 
 <script>
 export default {
+	data() {
+		return {
+			userData: {
+			email: null,
+			password: null
+		}
+		}
+	}, //data
+	methods: {
 
+		async submitForm(e) {
+			e.preventDefault();
+			const userData = this.userData;
+
+			console.log(`E-mail: ${userData.email}`)
+			console.log(`Password: ${userData.password}`)
+
+			//send request to the backend
+			console.log('Login!');
+	/* 		try {
+					await AuthService.logIn(userData);
+				} catch(err) {
+					console.log(err);
+			} */
+		}
+	}
 }
 </script>
 
-<style>
+<style scoped>
+
+    input[type="text"],
+    input[type="email"],
+    input[type="password"] {
+        border: 1px solid #333;
+        margin-left: 20px;
+    }
+
+    input[type="submit"] {
+        background: lightblue;
+    }
 
 </style>
