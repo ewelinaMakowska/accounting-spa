7ccf72848cbc27fafcf8a910712ef0aa91db2fca
Ewelina Makowska <ewelina.makowska0@gmail.com>
2020-09-08 19:49:20 +0200

    Update AuthController.js

--- server/src/controllers/AuthController.js
+++ server/src/controllers/AuthController.js
@@ -91,13 +91,12 @@ module.exports = {
       const userJson = user.toJSON();
 
       //store jwt in a httponly cookie
-      let cookieOptions = {
-        //expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000 ),
+ /*      let cookieOptions = {
         expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000 ),
         httpOnly: true,
-    }; 
+    };  */
 
-      return res
+  /*     return res
         .cookie('jwt', jwtRegUser(userJson), cookieOptions)
         .status(200).send({
           user: userJson,
@@ -105,7 +104,13 @@ module.exports = {
         })
         .json({
             msg: 'Successfully logged in',
-        });
+        }); */
+
+        return res.status(200).send({
+          user: userJson,
+          token: jwtRegUser(userJson)
+        })
+       
     
     } catch(err) {
       res.status(500).send({
