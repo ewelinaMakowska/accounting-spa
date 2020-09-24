be4c91022cceb1740ea8132942fb26fa1d5cd147
Ewelina Makowska <ewelina.makowska0@gmail.com>
2020-09-24 19:26:43 +0200

    new config I

---
+++ client/build/webpack.config.dev.js
@@ -0,0 +1,26 @@
+'use strict'
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    './src/main.js'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    })
+  ]
+}
