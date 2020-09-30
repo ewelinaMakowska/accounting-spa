2025440099e137247fe9aa83dfa64476c8885019
Ewelina Makowska <ewelina.makowska0@gmail.com>
2020-09-30 21:02:04 +0200

    jest

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -14,5 +14,13 @@ module.exports = {
   },
   parserOptions: {
     parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+    node: true,
+    mocha: true
+  },
+  globals: {
+    expect: true
   }
 }
