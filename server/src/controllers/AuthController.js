a864325241996f2d9d459c5b471952d3f992cde8
Ewelina Makowska <ewelina.makowska0@gmail.com>
2021-02-18 17:21:39 +0100

    a few comments deleted

--- server/src/controllers/AuthController.js
+++ server/src/controllers/AuthController.js
@@ -84,54 +84,19 @@ module.exports = {
         user = await loginHelper.getUserData(req);
         console.log(user)
         
-
-    /*     userData = await User.findOne({
-          where: {
-            email: email
-          }
-        }).then((userData) => {
-          console.log(`USER: ${userData}`)
-        }
-        ) */
-
         if(!user) {
           return res.status(403).send({
             error: 'Invalid login/email information'
           })
         } else { 
-
-       /*  //if user exists check if password matches
-        console.log(password)
-        console.log(user.password) */
-        
-        
-       //ASYNC
-    
-       /*  bcrypt.compare(password, user.password).then((err, resp) => {
-          console.log(resp)
-          if(resp) {
-            const userJson = user.toJSON();
-            return res.status(200).send({
-            user: userJson,
-            token: jwtRegUser(userJson)
-          })
-        } else {
-          console.log(password)
-          console.log(user.password)
-            return res.status(403).send({
-              error: 'Invalid password'
-            }) 
-          }               
-      });  */
-
-      //SYNC
-      let isPasswordValid = bcrypt.compareSync(password, user.password)      
-      if(isPasswordValid) {
-        res.status(200).send()
-      } else {
-        res.status(403).send()
-      }   
-      } //else
+          //SYNC
+          let isPasswordValid = bcrypt.compareSync(password, user.password)      
+          if(isPasswordValid) {
+            res.status(200).send()
+          } else {
+            res.status(403).send()
+          }   
+        } //else
       
       } catch(err) { 
         console.log(err)
