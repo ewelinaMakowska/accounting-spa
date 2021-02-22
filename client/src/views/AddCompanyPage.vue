3497ad7845567f4f4fca96f9a4a666eb1164a592
Ewelina Makowska <ewelina.makowska0@gmail.com>
2021-02-22 17:25:00 +0100

    autocomplete city input

--- client/src/views/AddCompanyPage.vue
+++ client/src/views/AddCompanyPage.vue
@@ -20,13 +20,33 @@
                 type="search"
                 placeholder="Miasto"
                 v-model.trim="city"
+                @keyup="loadCitiesFilteredLimited($event)"
+                @focus="setSearching()"
+                @blur="setSearchingFalse()"
+                id="city-input"
+                autocomplete="off"
                 />
-                <!-- todo: autocomplete with no city? go to add city! -->
-                <input 
-                type="text"
-                placeholder="województwo"
-                v-model.trim="region"
-                />
+
+                 <div class="autocomplete__wrapper" tabindex="0" v-show="(this.searching)">
+                  <div class="autocomplete">
+                    <ul class="autocomplete__list">
+                      <li
+                        v-for="(city, id) in cities"
+                        :key="id"
+                      >
+                        <button
+                          class="autocomplete_button"
+                          @click="autocompleteInput($event)"
+                        >                        
+                           {{ city.name }}, {{ city.region }}
+                          <span class="cityid" style="display:none;">{{city.id}}</span>
+                        </button>
+                      </li>
+                    </ul>
+                    <button>Dodaj miasto</button>
+                  </div> <!--div autocomplete -->
+                </div> <!-- /autocomplete wrapper -->
+
               </span>                
             </div>
             
@@ -171,7 +191,8 @@
         price: '',
         description: '',
         email: '',
-        additionalPoints: ''
+        additionalPoints: '',
+        searching: false
       }
     },
     methods: {
@@ -190,7 +211,70 @@
 
         console.log(data.name, data.city, data.region)
 
-      }
+      },
+       setSearching(){
+        this.searching = 1;
+        console.log('set searching true')
+      },
+
+      setSearchingFalse(){
+        setTimeout(() => {
+          this.searching = 0;
+          console.log('set searching false')
+        }, 500)
+      },
+
+      async loadCities () {
+        await this.$store.dispatch('loadCities')
+          .catch(function (error) {
+            console.log(error)
+          })
+        console.log(this.$store.getters.state.cities.citiesList)
+      },
+
+    //handler of a button with city name - ads cityid to url
+      async autocompleteInput ($event) {
+        const cityID = $event.target.childNodes[1].innerText;
+        const cityName = $event.target.innerText;
+        const searchInput = document.getElementById('city-input');
+
+
+        console.log($event.target)
+        console.log('')
+        console.log('')
+        console.log(cityID)
+        console.log(cityName)
+
+        searchInput.value = cityName;
+
+    /*     this.$router.push({ path: 'search', query: { city: cityID, page: 1}})
+        window.location.reload(); */
+
+      // await this.searchResults()
+       // document.getElementsByClassName('autocomplete')[0].classList.add('hidden')
+      },
+
+    //gets list of cities and shows it
+      async loadCitiesFilteredLimited($event) {
+        const name = document.getElementById('city-input').value;
+        console.log(`name: ${name}`)
+
+        await this.$store.dispatch('loadCitiesFilteredLimited', name)
+          .catch(function (error) {
+            console.log(error)
+          })
+
+        if ($event.target.value!=''){
+          this.searching = 1 
+        } else {
+          this.searching = 0
+        }
+      },
     },
+    computed: {
+      cities() {
+        return this.$store.getters.loadedCities
+      }
+    }
   }
 </script>
