0c90eda33cf1e92066fdbed82ee6be535eeb3131
Ewelina Makowska <ewelina.makowska0@gmail.com>
2021-03-02 18:44:47 +0100

    company thumbs icons

--- client/src/components/OfficeThumb.vue
+++ client/src/components/OfficeThumb.vue
@@ -2,14 +2,24 @@
   <div class="thumbs__thumb animate__animated animate__fadeIn">
     <div class="thumb__top">
       <div class="top__left">
-        <img src="assets/img/dummy-logo.svg" class="thumb__logo" alt="company logo" />
+        <img src="/assets/img/dummy-logo.svg" class="thumb__logo" alt="company logo" />
       </div>
-      <div class="top__right">      
+      <div class="top__right">   
+           
         <ul class="thumb__list">
           <li class="thumb__name">{{ name }}</li>
-          <li class="thumb__city">{{ city }}</li>
-          <li class="thumb__price">{{ price }}</li>
+
+          <li class="thumb__city">
+            <i class="material-icons-sharp checkmark thumb__icon">place</i>
+            <span class="thumb__city-wrapper">{{ city }}</span>
+          </li>
+
+          <li class="thumb__price">
+            <i class="material-icons-sharp checkmark thumb__icon">local_offer</i>
+            {{ price }} zł/msc
+          </li>
         </ul>
+
       </div>
     </div>
 
