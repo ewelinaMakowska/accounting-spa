16a85e974185bf62b565388994f1a850a8094600
Ewelina Makowska <ewelina.makowska0@gmail.com>
2021-03-13 14:41:41 +0100

    test if no auth header

--- server/tests/isAuth.spec.js
+++ server/tests/isAuth.spec.js
@@ -8,4 +8,14 @@ test('if malformed or no token it should throw an error', () => {
   }
 
   expect(() => {isAuth(req, () => {})}).toThrow(Error)
+}),
+
+test("if no auth header it should throw 'Not authenticated.", () => {
+  const req = {
+    get: () => {
+      return null;
+    }
+  }
+
+  expect(() => {isAuth(req, () => {})}).toThrow('Not authenticated.')
 })
