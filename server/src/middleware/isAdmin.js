aa4f48e326daf1d2c1437d03623071f2b8f16a18
Ewelina Makowska <ewelina.makowska0@gmail.com>
2021-03-15 12:31:52 +0100

    Update isAdmin.js

--- server/src/middleware/isAdmin.js
+++ server/src/middleware/isAdmin.js
@@ -5,24 +5,3 @@ module.exports = (req, res, next) => {
   res.status(401).send('Not authenticated.')
  }
 }
-
-
-/* test("it should throw 'Not authenticated.' error if user role is different than admin", () => {
-  const req = {
-    role: 'admin'
-  }
-
-  const res = {
-    status: function(code) {
-      this.statusCode = code;
-      return this;
-    },
-    send: function(message) {
-      return message;
-    }
-  }
-
-  expect(() => {
-    isAdmin(req, res, () => {}).toThrow('Not authenticated.')
-  })
-}), */
