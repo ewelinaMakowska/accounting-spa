84f50ef6c70aba682e80788da16003fb031c32a7
Ewelina Makowska <ewelina.makowska0@gmail.com>
2021-03-23 18:58:44 +0100

    test with closing db connection

--- server/tests/authController.spec.js
+++ server/tests/authController.spec.js
@@ -2,19 +2,23 @@ const AuthController = require('../src/controllers/AuthController')
 const bcrypt = require('bcryptjs');
 jest.mock('express-validator');
 const expressValidator = require('express-validator');
+const sequelize = require('sequelize')
+const models = require('../src/models');
 
 
 
 describe('if validation errors', () => {
   beforeEach(() => {  
-    expressValidator.validationResult.mockImplementation(() => ({
-      isEmpty: jest.fn().mockReturnValue(false),
-      array: jest.fn().mockReturnValue([{ test: 'error' }])
-    }));
+   
   })
 
 
   test("if login data are in wrong format it should throw 422 error", (done) => {  
+    expressValidator.validationResult.mockImplementation(() => ({
+      isEmpty: jest.fn().mockReturnValue(false),
+      array: jest.fn().mockReturnValue([{ test: 'error' }])
+    }));
+
     const res = {
       status: jest.fn(function(code) {
         return this
@@ -26,8 +30,12 @@ describe('if validation errors', () => {
     AuthController.login({}, res)
     .then(() => {
       try {
-        done()
-        expect(res.status).toHaveBeenCalledWith(422) 
+        expect(res.status).toHaveBeenCalledWith(422)
+        models.sequelize.sync()
+        .then(() => { models.sequelize.close()})
+        .then(() => {
+          done()
+        })
       } catch(err){
         done(err)
       }
